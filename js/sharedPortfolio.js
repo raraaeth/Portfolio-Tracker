d82b2cc21fadbfde572ee320e8f0f1392bad0b10
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-06-27 14:01:44 +0700

    Add shared portfolio structure in sharedPortfolio.js

--- js/sharedPortfolio.js
+++ js/sharedPortfolio.js
@@ -3,6 +3,28 @@
    File : sharedPortfolio.js
 ===================================================== */
 
+/* ===========================
+   SHARED PORTFOLIO
+=========================== */
+
+const Portfolio = {
+
+    prices:{},
+
+    balances:{},
+
+    wallets:{},
+
+    reserve:{
+
+        usd:0,
+
+        idr:0
+
+    }
+
+};
+
 
 
 
