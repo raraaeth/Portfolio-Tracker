dd26aa102a31cbd91e20ad8da6386208ee1b0e2d
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-06-27 20:51:45 +0700

    Add main.js for Portfolio Tracker initialization

---
+++ js/main.js
@@ -0,0 +1,82 @@
+/* =====================================================
+   Portfolio Tracker v3.0
+   File : main.js
+===================================================== */
+
+/* ===========================
+   INITIALIZE APP
+=========================== */
+
+document.addEventListener(
+
+    "DOMContentLoaded",
+
+    init
+
+);
+
+async function init(){
+
+    console.log(
+
+        APP.NAME,
+
+        "v"+APP.VERSION
+
+    );
+
+    /* ======================
+       LOAD DATA
+    ====================== */
+
+    await fetchTransactions();
+
+    await fetchPrices();
+
+    /* ======================
+       PROCESS
+    ====================== */
+
+    processData();
+
+    /* ======================
+       DASHBOARD
+    ====================== */
+
+    updateDashboard();
+
+    /* ======================
+       WALLET
+    ====================== */
+
+    updateWallet();
+
+    /* ======================
+       ACTIVITIES
+    ====================== */
+
+    updateActivities();
+
+    initActivityFilter();
+
+    /* ======================
+       AIRDROP
+    ====================== */
+
+    initAirdrop();
+
+    /* ======================
+       CHART
+    ====================== */
+
+    updateCharts();
+
+    console.log(
+
+        "Portfolio Loaded",
+
+        Portfolio
+
+    );
+
+}
